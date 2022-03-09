24120d0e57f1b9e79c77d3f34625d44ce89ea14b
sehumah <sehumah@protonmail.com>
2022-03-09 01:46:11 -0600

    add functionality to convert json objects to list of tweets and also to parse tweets received as json objects

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.kt
@@ -1,5 +1,30 @@
 package com.codepath.apps.restclienttemplate.models
 
+import org.json.JSONArray
+import org.json.JSONObject
+
 class Tweet {
 
+    var tweetText: String = ""
+    var createdAt: String = ""
+    var user: User? = null  // user who created the tweet
+
+    companion object {
+        fun parseJSON (jsonObject: JSONObject) : Tweet {
+            val tweet = Tweet()
+            tweet.createdAt = jsonObject.getString("created_at")
+            tweet.tweetText = jsonObject.getString("text")
+            tweet.user = User.parseJSON(jsonObject.getJSONObject("user"))
+            return tweet
+        }
+
+        /* convert list of JSON objects into list of tweets */
+        fun fromJSONArray (jsonArray: JSONArray) : List<Tweet> {
+            val tweets = ArrayList<Tweet>()
+            for (i in 0..jsonArray.length()) {
+                tweets.add(parseJSON(jsonArray.getJSONObject(i)))
+            }
+            return tweets
+        }
+    }
 }
