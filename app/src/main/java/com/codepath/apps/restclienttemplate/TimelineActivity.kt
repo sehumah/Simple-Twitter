0d3dd4fcbcc4816f2c810e74eca6b0bbb5b2f562
sehumah <sehumah@protonmail.com>
2022-03-09 02:41:09 -0600

    complete TimelineActivity code to display tweets on timeline

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
@@ -3,9 +3,13 @@ package com.codepath.apps.restclienttemplate
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.codepath.apps.restclienttemplate.models.Tweet
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import com.github.scribejava.apis.TwitterApi
 import okhttp3.Headers
+import org.json.JSONException
 
 
 private const val TAG = "TimelineActivity"
@@ -13,19 +17,37 @@ class TimelineActivity : AppCompatActivity() {
 
     private lateinit var client: TwitterClient  // declare client variable
 
+    private lateinit var rvTweets: RecyclerView
+    private lateinit var adapter: TweetsAdapter
+    private val tweets = ArrayList<Tweet>() // hold list of tweets to hold all the tweets we get from the API call
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timeline)
 
         client = TwitterApplication.getRestClient(this)  // initialize client variable
-        populateHomeTimeline()
 
+        rvTweets = findViewById(R.id.rv_tweets)  // get the RecyclerView
+        adapter = TweetsAdapter(tweets)  // initialize adapter
+        rvTweets.layoutManager = LinearLayoutManager(this)  // give RecyclerView a LayoutManager
+        rvTweets.adapter = adapter  // set the adapter for the RecyclerView
+
+        populateHomeTimeline()
     }
 
     private fun populateHomeTimeline () {
         client.getHomeTimeline(object : JsonHttpResponseHandler(){
 
-            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON?) {
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
+                val jsonArray = json.jsonArray  // get json array from response
+                try {
+                    val listOfTweetsRetrieved = Tweet.fromJSONArray(jsonArray)  // parse jsonArray and make list of tweets
+                    tweets.addAll(listOfTweetsRetrieved)  // add it to our list of original tweets
+                    adapter.notifyDataSetChanged()  // notify the adapter that things have changed
+                } catch (e: JSONException) {
+                    Log.e(TAG, "JSON Exception: $e")
+                }
+
                 Log.i(TAG, "success populating home timeline. JSON object: $json")
             }
 
