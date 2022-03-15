fe332332392f75658fab7379b53ed6f65977b4ef
sehumah <sehumah@protonmail.com>
2022-03-14 22:44:49 -0500

    set on click listener for to each tweet

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.kt
@@ -1,27 +1,42 @@
 package com.codepath.apps.restclienttemplate
 
+import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.codepath.apps.restclienttemplate.models.Tweet
 
 
+private const val TAG = "TweetsAdapter"
 /* turn tweets objects to something that the Recycler view can display */
-class TweetsAdapter (private val tweets: ArrayList<Tweet>) : RecyclerView.Adapter<TweetsAdapter.ViewHolder>() {
+class TweetsAdapter (private val context: Context, private val tweets: ArrayList<Tweet>) : RecyclerView.Adapter<TweetsAdapter.ViewHolder>() {
 
-    class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
         val ivProfileImage: ImageView = itemView.findViewById(R.id.iv_profile_image)
         val tvUsername: TextView = itemView.findViewById(R.id.tv_username)
-        val tvScreenname: TextView = itemView.findViewById(R.id.tv_screen_name)
+        val tvScreenName: TextView = itemView.findViewById(R.id.tv_screen_name)
         val tvTimestamp: TextView = itemView.findViewById(R.id.tv_timestamp)
         val tvTweetText: TextView = itemView.findViewById(R.id.tv_tweet_body)
         // val tvReplyCount: TextView = itemView.findViewById(R.id.tv_reply_count)
         val tvRetweetCount: TextView = itemView.findViewById(R.id.tv_retweet_count)
         val tvFavoriteCount: TextView = itemView.findViewById(R.id.tv_favorite_count)
+
+        // register a tweet item with an onclick listener
+        init {
+            itemView.setOnClickListener (this)
+        }
+
+        override fun onClick(view: View?) {
+            val tweet = tweets[absoluteAdapterPosition]  // the tweet that was tapped/touched
+            Toast.makeText(context, tweet.user?.name, Toast.LENGTH_SHORT).show()  // notify that tweet was touched
+            Log.i(TAG, "in TweetAdapter's inner class.onClick. adapterPosition: $adapterPosition, layoutPosition: $layoutPosition, bindingAdapterPosition: $bindingAdapterPosition")
+        }
     }
 
     /* responsible for inflating the layout we want to use for each of the items to display in the RecyclerView */
@@ -41,7 +56,7 @@ class TweetsAdapter (private val tweets: ArrayList<Tweet>) : RecyclerView.Adapte
 
         // Set specific view items based on the specific tweet. Set item views based on views and data model
         holder.tvUsername.text = tweet.user?.name  // username might be null
-        holder.tvScreenname.text = "@${tweet.user?.screenName}"
+        holder.tvScreenName.text = "@${tweet.user?.screenName}"
         holder.tvTweetText.text = tweet.tweetText
         holder.tvTimestamp.text = tweet.getFormattedTimestamp()
         // holder.tvReplyCount.text = tweet.replyCount.toString()
@@ -67,5 +82,4 @@ class TweetsAdapter (private val tweets: ArrayList<Tweet>) : RecyclerView.Adapte
         tweets.addAll(tweetList)
         notifyDataSetChanged()
     }
-
 }
