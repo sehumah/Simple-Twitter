29ed51dc195d709f5d7c3acdad41d3e7c63ef4d9
sehumah <sehumah@protonmail.com>
2022-03-17 00:55:55 -0500

    create activity file for tweet detail activity

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetDetailActivity.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetDetailActivity.kt
@@ -3,6 +3,7 @@ package com.codepath.apps.restclienttemplate
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class TweetDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
