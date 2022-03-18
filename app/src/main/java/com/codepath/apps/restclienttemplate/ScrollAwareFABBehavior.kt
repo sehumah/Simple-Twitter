063a4eef1d22d23b86318c105d01ad1c33b97883
sehumah <sehumah@protonmail.com>
2022-03-18 15:45:27 -0500

    implement a constructor to enable layout inflation to work correctly

--- app/src/main/java/com/codepath/apps/restclienttemplate/ScrollAwareFABBehavior.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ScrollAwareFABBehavior.kt
@@ -1,12 +1,16 @@
 package com.codepath.apps.restclienttemplate
 
+import android.content.Context
+import android.util.AttributeSet
 import android.view.View
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.ViewCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.floatingactionbutton.FloatingActionButton.OnVisibilityChangedListener
 
-class ScrollAwareFABBehavior : FloatingActionButton.Behavior() {
+class ScrollAwareFABBehavior : FloatingActionButton.Behavior {
+
+    constructor (context: Context, attributeSet: AttributeSet) : super()
 
     // extend FloatingActionButton.Behavior to signal that we wish to handle scroll events in the vertical direction
     override fun onStartNestedScroll (
