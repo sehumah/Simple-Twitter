bb7ddea33c773191c97514175983ed7ddf94ca79
sehumah <sehumah@protonmail.com>
2022-03-18 15:33:07 -0500

    add functions to check the scroll direction and animate the floating action button

--- app/src/main/java/com/codepath/apps/restclienttemplate/ScrollAwareFABBehavior.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ScrollAwareFABBehavior.kt
@@ -1,11 +1,14 @@
 package com.codepath.apps.restclienttemplate
 
 import android.view.View
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.ViewCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.floatingactionbutton.FloatingActionButton.OnVisibilityChangedListener
 
 class ScrollAwareFABBehavior : FloatingActionButton.Behavior() {
+
+    // extend FloatingActionButton.Behavior to signal that we wish to handle scroll events in the vertical direction
     override fun onStartNestedScroll (
         coordinatorLayout: CoordinatorLayout,
         child: FloatingActionButton,
@@ -19,5 +22,34 @@ class ScrollAwareFABBehavior : FloatingActionButton.Behavior() {
                     coordinatorLayout, child, directTargetChild, target,
                     nestedScrollAxes, type
                 )
+    }
+
+
+    // check the Y position and determine whether to animate the button in or out
+    override fun onNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: FloatingActionButton,
+        target: View,
+        dxConsumed: Int,
+        dyConsumed: Int,
+        dxUnconsumed: Int,
+        dyUnconsumed: Int,
+        type: Int
+    ) {
+        super.onNestedScroll(
+            coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed,
+            dyUnconsumed, type
+        )
+        if (dyConsumed > 0 && child.visibility == View.VISIBLE) {
+            child.hide(object : OnVisibilityChangedListener() {
+                override fun onHidden(floatingActionButon: FloatingActionButton) {
+                    super.onHidden(floatingActionButon)
+                    floatingActionButon.visibility = View.INVISIBLE
+                }
+            })
+        }
+        else if (dyConsumed < 0 && child.visibility != View.VISIBLE) {
+            child.show()
+        }
     }
 }
